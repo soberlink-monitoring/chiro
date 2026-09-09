c7fe400a8e722ac94d3d535a0d771889478f9f3a
soberlink-monitoring <ttran@soberlink.com>
2026-09-08 22:27:13 -0700

    Delete app directory

--- app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { buildClearAdminCookie } from "@/lib/adminAuth";
-
-export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  res.headers.set("Set-Cookie", buildClearAdminCookie());
-  return res;
-}
